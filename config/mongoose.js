2c109eca6b0f66c87687ef46a6a0c8c0e8e94203
Manoj_Pant <manojpant097@gmail.com>
2023-11-08 19:49:36 +0530

    connected to cloud Database

---
+++ config/mongoose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const DB = 'mongodb+srv://manojpant097:manoj1234@hospitaldb.flvmdld.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+require('./config/mongoose');
 
 app.listen(port,(error)=>{
 
     if(error){
-        console.log(`Error in creating server and the error is: ${error}`);
+        console.log(`Error in creating server. Error is: ${error}`);
     }
     console.log(`Server is up and running on the port: ${port}`);
 })
