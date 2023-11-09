e3eb0aa763da0fd1640f9fae2a52000311a22ce8
Manoj_Pant <manojpant097@gmail.com>
2023-11-09 14:09:44 +0530

    created routes for patient

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+const status= require('../controllers/status')
+
+
 router.use('/doctor', require('./doctor'));
 
+router.use('/patients', require('./patients'));
+router.get('/reports/:status',passport.authenticate('jwt',{session:false}),status.filteredReports);
 
 module.exports = router;
